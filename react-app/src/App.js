70a1536538eba1b4fefaf233bfff1759fc46c6bd
disk042 <rey.disk042@gmail.com>
2021-02-26 21:48:46 +0900

    learn prop-types

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
 
 
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {name: "NoName"}
+    {name: "NoName", age: 3}
 
   ]
   return (
@@ -23,8 +25,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age:1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
